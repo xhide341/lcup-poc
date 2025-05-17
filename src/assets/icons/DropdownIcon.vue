c513497f91a06a2225a33cbb1795e8722fda59b9
xhide341 <shawnehgn10@gmail.com>
2025-05-17 18:37:11 +0800

    chore: add and delete assets

---
+++ src/assets/icons/DropdownIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="0" y="0" width="24" height="24" fill="transparent" />
+    <path
+      d="M19 9L12 16L5 9"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: middle;
+  cursor: pointer;
+  pointer-events: all;
+}
+</style>
---
+++ src/assets/icons/HamburgerIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    class="menu-icon"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3 17H21M3 12H21M3 7H21"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: middle;
+  color: inherit;
+  cursor: pointer;
+}
+</style>
